b07d0085e5ea79b1eecbd9473837d73aedbd7643
BrunoWalysOSF <bruno.walyson@osf.digital.com>
2023-04-10 09:17:48 -0300

    Adicionado um exception
    caso aja erro ao envio para kafka

--- ms-pessoa-producer/src/main/java/com/example/mspessoaproducer/services/PessoaProducerService.java
+++ ms-pessoa-producer/src/main/java/com/example/mspessoaproducer/services/PessoaProducerService.java
@@ -3,8 +3,10 @@ package com.example.mspessoaproducer.services;
 import com.example.mspessoaproducer.models.PessoaDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.Serializable;
 
@@ -16,8 +18,11 @@ public class PessoaProducerService {
     private final KafkaTemplate<String, Serializable> kafkaTemplate;
 
     public void sendPessoa(PessoaDTO pessoa){
-        log.info("Enviando : " + pessoa.getNome());
-        kafkaTemplate.send("ms-producer", pessoa);
-
+        try {
+            log.info("Enviando para kafka : " + pessoa.getNome());
+            kafkaTemplate.send("ms-producer", pessoa);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao enviar mensagem para o Kafka", e);
+        }
     }
 }
